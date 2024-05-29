05455286af528d8d485f94579126b686dff3b3a0
Denys Mykhailov <necuknpocto@gmail.com>
2024-05-30 00:19:07 +0300

    user interacrion for better experience

--- Solution/Program.py
+++ Solution/Program.py
@@ -28,24 +28,23 @@ star = np.array([
 ])
 
 
-def plot_shape(title: str, shape: np.array) -> None:
+def plot_shape(ax, title: str, shape: np.array) -> None:
     """
     Plot the shape with the given title (string) and shape (numpy array)
     """
-    #               X            Y
-    plt.plot(shape[:, 0], shape[:, 1])
-    plt.title(title)
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.grid(True)
-    plt.axis('equal')
+    ax.clear()
+    ax.plot(shape[:, 0], shape[:, 1])
+    ax.set_title(title)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.grid(True)
+    ax.axis('equal')
 
 
 def rotate(shape: np.array, angle: float) -> np.array:
     """
     Rotate the shape by the given angle (in degrees)
     """
-    # to radians
     angle = np.radians(angle)
 
     cos = np.cos(angle)
@@ -61,28 +60,51 @@ def rotate(shape: np.array, angle: float) -> np.array:
     return rotated_shape.T
 
 
-def main():
-    plt.figure(figsize=(10, 5))
-
-    # for badman
-    plt.subplot(1, 2, 1)
-    plot_shape("Batman", batman)
-
-    # second plot
-    plt.subplot(1, 2, 2)
-    plot_shape("Star", star)
-
-    plt.show(block=False)
-    plt.pause(2)
-    plt.clf()
-
-    plt.subplot(1, 2, 1)
-    plot_shape("Batman", batman)
-
-    plt.subplot(1, 2, 2)
-    plot_shape("Batman", rotate(batman, 180))
-    plt.show()
+def key_press(event):
+    global current_transformation, current_original
+    if event.key == 'right':
+        current_transformation = (current_transformation + 1) % len(transformations)
+    elif event.key == 'left':
+        current_transformation = (current_transformation - 1) % len(transformations)
+    elif event.key == 'a':
+        current_original = (current_original + 1) % len(originals)
+    elif event.key == 'd':
+        current_original = (current_original - 1) % len(originals)
+    elif event.key == 'z':
+        current_transformation = (current_transformation + 1) % len(transformations)
+        current_original = (current_original + 1) % len(originals)
+    elif event.key == 'x':
+        current_transformation = (current_transformation - 1) % len(transformations)
+        current_original = (current_original - 1) % len(originals)
+    update_plot()
 
 
-if __name__ == "__main__":
-    main()
+def update_plot():
+    ax1.clear()
+    ax2.clear()
+    title_og, original_shape = originals[current_original]
+    plot_shape(ax1, title_og, original_shape)
+
+    title_t, transformed_shape = transformations[current_transformation]
+    plot_shape(ax2, title_t, transformed_shape)
+    plt.draw()
+
+
+originals = [
+    ("Original Batman", batman),
+    ("Original Star", star)
+]
+transformations = [
+    ("Rotated Batman 180 degrees", rotate(batman, 180)),
+    ("Rotated Star 90 degrees", rotate(star, 90))
+]
+
+current_original = 0
+current_transformation = 0
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+plot_shape(ax1, originals[0][0], originals[0][1])
+plot_shape(ax2, transformations[0][0], transformations[0][1])
+
+fig.canvas.mpl_connect('key_press_event', key_press)
+plt.show()
--- Solution/README.md
+++ Solution/README.md
@@ -1,3 +1,6 @@
 Used sources:
-- [numpy](https://numpy.org/doc/stable/reference/generated/numpy.rot90.html)
+- [numpy angles](https://numpy.org/doc/stable/reference/generated/numpy.rot90.html)
 - [rotate matrix](https://www.geeksforgeeks.org/numpy-radians-deg2rad-python/)
+- [plt axes](https://www.geeksforgeeks.org/matplotlib-axes-class/)
+- [Updating](https://youtu.be/PpzaVR3PaBY) and there were other vids...
+- [mpl_connect](https://matplotlib.org/stable/users/event_handling.html) + [ChatGPT](https://chat.openai.com/)(only for the purpose to interact with the user!!)
